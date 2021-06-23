47cbd08ef63cc31a5dd0853c2c75c97f75f29699
Bendik Arbogast <arbobendik@gmail.com>
2021-06-23 23:34:15 +0200

    improved raytraced shading performance

--- static/shaders/fragment.glsl
+++ static/shaders/fragment.glsl
@@ -14,44 +14,33 @@ uniform int world_tex_height;
 
 out vec4 outColor;
 
-bool sameVector(vec3 a, vec3 b)
-{
-  return equal(a, b) != bvec3(true, true, true);
-}
-
-bool sameSide(vec3 p1, vec3 p2, vec3 a, vec3 b)
-{
-    vec3 cp1 = cross(b-a, p1-a);
-    vec3 cp2 = cross(b-a, p2-a);
-    return (dot(cp1, cp2) >= 0.0);
-}
-
-bool isPointInTriangle(vec3 p, vec3 a, vec3 b, vec3 c)
-{
-    return sameSide(p,a, b,c) && sameSide(p,b, a,c) && sameSide(p,c, a,b);
-}
-
-bool shadowTrace(vec3 n, vec3 l, vec3 p, mat3 t)
+bool shadowTrace(vec3 n, vec3 l, vec3 p, mat3 t) // normal, light source, pixel position, triangle.
 {
   vec3 r = normalize(l - p);
-  if(dot(n, r) == 0.0)
-  {
-    return false;
-  }
+  // Test if ray and plane are parallel.
+  if(dot(n, r) == 0.0) return false;
   float s = dot(n , t[0] - p) / dot(n, r);
-  if (s > length(l - p) || s <= 0.0)
-  {
-    return false;
-  }
+  // Ensure that ray triangle intersection is between light source and texture.
+  if (s > length(l - p) || s <= 0.0) return false;
+  // Calculate intersection point.
   vec3 c = s * r + p;
-  return isPointInTriangle(c, t[0], t[1], t[2]);
+  // Pre calculate differences.
+  vec3 t10 = t[1] - t[0];
+  vec3 t20 = t[2] - t[0];
+  vec3 t21 = t[2] - t[1];
+  vec3 ct0 = c - t[0];
+  // Test if point on plane is in Triangle by looking for each edge if point is in or outside.
+  if (dot(cross(t21, c-t[1]), cross(t21, -t10)) < 0.0) return false;
+  if (dot(cross(t20, ct0), cross(t20, t10)) < 0.0) return false;
+  if (dot(cross(t10, ct0), cross(t10, t20)) < 0.0) return false;
+  return true;
 }
 
 void main()
 {
   ivec2 size = textureSize(world_tex, 0);
   bool in_shadow = false;
-  for(int i = 0; i < size.y && in_shadow == false; ++i)
+  for(int i = 0; i < size.y && in_shadow == false; i++)
   {
     mat3 t = mat3(
       texelFetch(world_tex, ivec2(0, i), 0).xyz,
@@ -59,11 +48,9 @@ void main()
       texelFetch(world_tex, ivec2(2, i), 0).xyz
     );
     vec3 n = texelFetch(world_tex, ivec2(4, i), 0).xyz;
-    if(n == normal || sameVector(t[0], triangle[0]) || sameVector(t[1], triangle[1]) || sameVector(t[2], triangle[2]))
-    {
-      in_shadow = shadowTrace(n, light, position, t);
-    }
+    in_shadow = shadowTrace(n, light, position, t);
   }
+
   if (in_shadow)
   {
     outColor = vec4(0.2 * color.xyz, color.w);
