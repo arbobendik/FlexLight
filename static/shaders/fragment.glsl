f56dec7a02931782ff69540bc71b5afbbce818a8
Bendik Arbogast <arbobendik@gmail.com>
2021-06-23 23:01:15 +0200

    fixed self shadowing of elements

--- static/shaders/fragment.glsl
+++ static/shaders/fragment.glsl
@@ -14,6 +14,10 @@ uniform int world_tex_height;
 
 out vec4 outColor;
 
+bool sameVector(vec3 a, vec3 b)
+{
+  return equal(a, b) != bvec3(true, true, true);
+}
 
 bool sameSide(vec3 p1, vec3 p2, vec3 a, vec3 b)
 {
@@ -34,13 +38,12 @@ bool shadowTrace(vec3 n, vec3 l, vec3 p, mat3 t)
   {
     return false;
   }
-  vec3 a = t[0] - p;
-  float s = (n.x * a.x + n.y * a.y + n.z * a.z) / (n.x * r.x + n.y * r.y + n.z * r.z);
-  if (s > length(l - p))
+  float s = dot(n , t[0] - p) / dot(n, r);
+  if (s > length(l - p) || s <= 0.0)
   {
     return false;
   }
-  vec3 c = p + s * r;
+  vec3 c = s * r + p;
   return isPointInTriangle(c, t[0], t[1], t[2]);
 }
 
@@ -48,22 +51,22 @@ void main()
 {
   ivec2 size = textureSize(world_tex, 0);
   bool in_shadow = false;
-  for(int i = 0; i < 12 && in_shadow == false; ++i)
+  for(int i = 0; i < size.y && in_shadow == false; ++i)
   {
     mat3 t = mat3(
       texelFetch(world_tex, ivec2(0, i), 0).xyz,
       texelFetch(world_tex, ivec2(1, i), 0).xyz,
       texelFetch(world_tex, ivec2(2, i), 0).xyz
     );
-    if(t!=triangle)
+    vec3 n = texelFetch(world_tex, ivec2(4, i), 0).xyz;
+    if(n == normal || sameVector(t[0], triangle[0]) || sameVector(t[1], triangle[1]) || sameVector(t[2], triangle[2]))
     {
-      vec3 n = texelFetch(world_tex, ivec2(4, i), 0).xyz;
       in_shadow = shadowTrace(n, light, position, t);
     }
   }
   if (in_shadow)
   {
-    outColor = vec4(0.3 * color.xyz, color.w);
+    outColor = vec4(0.2 * color.xyz, color.w);
   }
   else
   {
