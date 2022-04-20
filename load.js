4d2fd87fecc40d8c84024420102d923a4eac2726
5ubstitute <fabianschwarz3@aol.com>
2022-04-20 07:59:50 +0200

    Changed from cache select element to GET-Request form

--- load.js
+++ load.js
@@ -1,37 +1,26 @@
 "use strict";
 
-(function(select) {
+(function(search) {
 	document.currentScript.remove();
 
-	const options = select.options;
-	if (options.selectedIndex !== 0) {
-		start();
+	if (search.has("v")) {
+		const script = document.createElement("script");
+		script.src = "scripts/" + search.get("v") + ".js";
+		document.head.appendChild(script);
+		document.addEventListener("DOMContentLoaded", function() {
+			while (document.body.childNodes.length > 0) document.body.firstChild.remove();
+		}, {once: true});
+		
 		return;
 	}
 
-	select.addEventListener("change", function() {
-		location.reload();
+	window.addEventListener("DOMContentLoaded", function() {
+		const form = document.getElementById("form");
+		form.addEventListener("change", function() {
+			console.log(this);
+			if (this[0].value !== "Scenes") {
+				this.submit();
+			}
+		});
 	}, {once: true});
-
-	function start() {
-		while (document.body.childNodes.length > 0) document.body.firstChild.remove();
-		const styleSheet = document.createElement("link");
-		styleSheet.href = "style.css";
-		styleSheet.rel = "stylesheet";
-		document.head.appendChild(styleSheet);
-
-		const ioScript = document.createElement("script");
-		ioScript.src = "io.js";
-		document.head.appendChild(ioScript);
-
-		const rayTracerScript = document.createElement("script");
-		rayTracerScript.src = "raytracer.js";
-		document.head.appendChild(rayTracerScript);
-
-		const script = document.createElement("script");
-		script.src = "scripts/" + select.value + ".js";
-		document.head.appendChild(script);
-
-		document.title = options[options.selectedIndex].textContent;
-	}
-})(document.getElementById("select"));
+})(new URLSearchParams(location.search));
